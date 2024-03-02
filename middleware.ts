a465ba7731394f1f2035dded02e1ce165ea83d41
wiesty <basti@hypermail.de>
2024-03-02 16:12:28 +0100

    added cms for scripts and style src header middleware

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ export function middleware(request: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}';
+    script-src 'self' ${process.env.CMS_DOMAIN} 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' ${process.env.CMS_DOMAIN} 'nonce-${nonce}';
     img-src 'self' blob: data: ${process.env.CMS_DOMAIN};
     font-src 'self';
     object-src 'none';
