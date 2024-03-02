ce1c6633717744c9c807b1acfa13064323ffcf27
wiesty <basti@hypermail.de>
2024-03-02 16:06:23 +0100

    added cms domain for blog posts

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
     style-src 'self' 'nonce-${nonce}';
-    img-src 'self' blob: data:;
+    img-src 'self' blob: data: ${process.env.CMS_DOMAIN};
     font-src 'self';
     object-src 'none';
     base-uri 'self';
