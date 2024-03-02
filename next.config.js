f352e5b8968b3774f2f1c785fc6be6d2499082ee
wiesty <basti@hypermail.de>
2024-03-02 15:55:05 +0100

    updated security headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    script-src 'self' 'unsafe-eval';
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
@@ -11,7 +11,7 @@ const cspHeader = `
     frame-ancestors 'none';
     block-all-mixed-content;
     upgrade-insecure-requests;
-    X-Frame-Options 'SAMEORIGIN';
+    require-trusted-types-for 'script';
 `
 
 const nextConfig = {
