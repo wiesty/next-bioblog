0278279e78b1c433bea604a62e2346d3bead068f
wiesty <basti@hypermail.de>
2024-03-03 00:19:15 +0100

    new blog design and disclaimer

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -5,7 +5,7 @@ import { FaAngleLeft } from "react-icons/fa";
 import parse from "html-react-parser";
 import { Badge } from "@/components/ui/badge";
 import { Metadata } from "next";
-import { notFound } from "next/navigation"
+import { notFound } from "next/navigation";
 import { fetchConfig } from "@/lib/fetchconfig";
 import { fetchBlog } from "@/lib/fetchblogbyid";
 
@@ -40,9 +40,9 @@ export async function generateMetadata({ params }: any): Promise<Metadata> {
   }
 }
 
-
 const BlogPage = async ({ params }: any) => {
   const blog = await fetchBlog(params.id);
+  const config = await fetchConfig();
 
   if (blog.data === null) {
     notFound();
@@ -55,47 +55,56 @@ const BlogPage = async ({ params }: any) => {
 
   return (
     <main>
-      <div className="flex justify-between items-center">
-        <div className="flex items-center">
-          <Link href="/" className="flex items-center">
+      <div className="relative rounded-lg overflow-hidden">
+        {imageUrl !== "" && (
+          <div
+            className="absolute inset-0 z-0 bg-cover bg-center filter blur-sm"
+            style={{ backgroundImage: `url(${imageUrl})`, opacity: 0.4 }}
+          ></div>
+        )}
+        <div className="absolute mt-2 z-10 top-0 left-0 ml-2">
+          <Link href="/" className="flex z-10 items-center">
             <FaAngleLeft />
             <span className="ml-1">back</span>
           </Link>
         </div>
-        <div className="items-end text-end">
-        {blog.data.attributes.categories.data.map((category: any) => (
+        <div className="absolute mt-2 z-10 top-0 right-0 mr-2">
+          <div className="flex z-10 items-center">
+            {blog.data.attributes.categories.data.map((category: any) => (
               <React.Fragment key={category.id}>
                 <Link href={`/categories/${category.id}`}>
-                  <Badge variant="destructive">{category.attributes.Title}</Badge>{" "}
+                  <Badge variant="destructive">
+                    {category.attributes.Title}
+                  </Badge>{" "}
                 </Link>
               </React.Fragment>
             ))}
-        </div>
-      </div>
-      <div>
-        {imageUrl !== "" && (
-          <div className="relative w-full h-96 overflow-hidden rounded-lg mt-5">
-            <Image
-              priority
-              width={1280}
-              height={720}
-              src={imageUrl}
-              alt={"Blog post image"}
-            />
           </div>
-        )}
-        <div className="mt-8">
-          <h1 className="text-3xl font-semibold text-primary">
+        </div>
+        <div className="relative min-h-52 flex justify-between items-center px-4 py-2">
+          <h1 className="text-2xl font-semibold text-primary text-center ml-2 mr-2 break-words">
             {blog.data.attributes.Title}
           </h1>
+        </div>
+      </div>
+
+      <div>
+        <div className="mt-8">
           <p className="text-gray-600 mt-6">
             {blog.data.attributes.Description}
           </p>
           <div className="mt-8">{parse(blog.data.attributes.Content)}</div>
-          <div className="mt-4 flex items-center text-gray-400 mb-8">
+          <div className="mt-4 flex flex-col items-center text-gray-400">
             <span className="text-sm">
               published{" "}
               {new Date(blog.data.attributes.publishedAt).toLocaleString()}
+            </span>
+            <span className="text-sm">
+              updated{" "}
+              {new Date(blog.data.attributes.updatedAt).toLocaleString()}
+            </span>< br/>
+            <span className="text-sm w-72 text-center">
+            {config.data.attributes.disclaimer}
             </span>
           </div>
         </div>
--- middleware.tsx
+++ middleware.tsx
@@ -11,7 +11,7 @@ export function middleware(request: NextRequest) {
   const cspHeader = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' ${process.env.CMS_DOMAIN} 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}' ${process.env.CMS_DOMAIN};
+    style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data: ${process.env.CMS_DOMAIN};
     font-src 'self';
     object-src 'none';
