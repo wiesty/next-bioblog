543308008d90471c340a97674b485915d76055c1
wiesty <basti@hypermail.de>
2024-03-02 15:58:52 +0100

    security headers to middleware

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    block-all-mixed-content;
-    upgrade-insecure-requests;
-    require-trusted-types-for 'script';
-`
 
 const nextConfig = {
     images: {
@@ -28,19 +14,6 @@ const nextConfig = {
           pathname: '**',
         },
       ],
-    },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'Content-Security-Policy',
-              value: cspHeader.replace(/\n/g, ''),
-            },
-          ],
-        },
-      ]
     },
   };
   
